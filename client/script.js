d1e0925e9785e97ec5ecfda1833c178041ff4759
Reyier <adamreyier@gmail.com>
2024-01-08 11:49:38 +0100

    update

--- client/script.js
+++ client/script.js
@@ -1,86 +1,122 @@
-/*const url = "http://localhost:3000/users";
+
+const url = "http://localhost:3000/cars";
+const carListContainer = document.getElementById("carList");
 
 fetch(url)
-    .then((response) => response.json())
-    .then((users) => {
-        const ul = document.createElement("ul");
-        ul.classList.add("userList");
-        const div = document.querySelector(".container");
-        div.insertAdjacentElement("beforeend", ul);
+  .then((response) => response.json())
+  .then((cars) => {
+    const ul = document.createElement("ul");
+    ul.classList.add("carList");
 
-        users.forEach(user => {
-            const li = document.createElement("li");
-            li.style.backgroundColor = user.color;
-            const firstName = user.firstName;
-            const lastName = user.lastName;
-            const userName = user.username;
-            const br = `<br/>`;
-            const html = `<li>Name: ${firstName} ${lastName} ${br}Username: ${userName}</>`;
-            ul.insertAdjacentElement('beforeend',li);
-            li.insertAdjacentHTML('beforeend', html);
-        })
-        
+    cars.forEach((car) => {
+      const li = document.createElement("li");
+      li.style.backgroundColor = car.inputColor;
+      const model = car.model;
+      const year = car.year;
+      const gear = car.gear;
+      const fuel = car.fuel;
+      const color = car.color;
+      const mileage = car.mileage;
+
+      const html = `<li>Name: ${model} ${year} ${gear} ${fuel} ${color} ${mileage}</li>`;
+
+      ul.insertAdjacentElement("beforeend", li);
+      li.insertAdjacentHTML("beforeend", html);
     });
+    carListContainer.appendChild(ul);
+  })
 
-var checkboxElement = document.getElementById("divStyle")
+  .catch((error) => {
+    console.error("Error fetching cars:", error);
+  });
 
-var textElements = document.getElementsByClassName("textfield")
+// 3 MÖJLIGGÖRA UPPDATERING AV RESURS
+document.addEventListener("DOMContentLoaded", () => {
+  const editButton = document.getElementById("editButton"); 
 
-var buttonElement = document.getElementsByTagName("button") [0]
+  editButton.addEventListener("click", () => {
+   
+    const resourceId = 1; 
 
-var outputElement = document.getElementsByClassName("output") [0]
+   
+    fetch(`http://localhost:3000/resource/${resourceId}`)
+      .then((response) => response.json())
+      .then((resource) => {
+    
+        document.getElementById("inputModel").value = resource.model;
+        document.getElementById("inputYear").value = resource.year;
+        document.getElementById("inputGear").value = resource.gear;
+        document.getElementById("inputFuel").value = resource.fuel;
+        document.getElementById("inputColor").value = resource.color;
+        document.getElementById("inputMileage").value = resource.mileage;
 
-checkboxElement.addEventListener("change", () => {
-    outputElement.style.backgroundColor = textElements [0].value
-})
-
-buttonElement.addEventListener("click", () => {
-    outputElement.remove()
-}
-)
-
-function onInput (event) {
-    console.log("Input target", event.target)
-
-    const targetName = event.target.name
-    if(targetName === "content"){
-        outputElement.innerHTML = textElements[1].value
-    }
-} */
-
-
- const url = "http://localhost:3000/cars";
- const carListContainer = document.getElementById('carList');
-
-fetch(url)
-    .then((response) => response.json())
-    .then((cars) => {
-        const ul = document.createElement("ul");
-        ul.classList.add("carList");
-
-        
-        cars.forEach(car => {
-            const li = document.createElement("li");
-            li.style.backgroundColor = car.inputColor;
-            const model = car.model;
-            const year = car.year;
-            const gear = car.gear;
-            const fuel = car.fuel;
-            const color = car.color;
-            const mileage = car.mileage;
-            
-            const html = `<li>Name: ${model} ${year} ${gear} ${fuel} ${color} ${mileage}</li>`;
-            
-            ul.insertAdjacentElement('beforeend',li);
-            li.insertAdjacentHTML('beforeend', html);
-        })
-        carListContainer.appendChild(ul);
-    })
-
-    .catch((error) => {
-        console.error('Error fetching cars:', error);
+       
+        localStorage.setItem("currentResourceId", resourceId);
+      })
+      .catch((error) => {
+        console.error("Error fetching resource:", error);
       });
+  });
+});
 
 
 
-      // Att göra 2. VISA ALLA (R I CRUD)
+const form = document.getElementById("carForm");
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  submitForm();
+});
+
+function submitForm() {
+  const formData = new FormData();
+
+  formData.append('additionalData', 'additionalValue');
+
+  const model = document.getElementById("inputModel").value;
+  const year = document.getElementById("inputYear").value;
+  const gear = document.getElementById("inputGear").value;
+  const fuel = document.getElementById("inputFuel").value;
+  const color = document.getElementById("inputColor").value;
+  const mileage = document.getElementById("inputMileage").value;
+
+  formData.append('model', model);
+  formData.append('year', year);
+  formData.append('gear', gear);
+  formData.append('fuel', fuel);
+  formData.append('color', color);
+  formData.append('mileage', mileage);
+
+  console.log("Form submitted!");
+
+  submitFormData(formData);
+}
+
+function submitFormData(formData) {
+  fetch("http://localhost:3000/cars", {
+    method: "POST",
+    body: formData,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Data sent successfully:", data);
+    })
+    .catch((error) => {
+      console.error("There was an error sending the data:", error);
+    });
+}
+
+const clearButton = document.querySelector("#carForm button.btn-danger");
+clearButton.addEventListener("click", function(e) {
+  e.preventDefault();
+  clearFields();
+});
+
+function clearFields() {
+  document.getElementById("inputModel").value = "";
+  document.getElementById("inputYear").value = "";
+  document.getElementById("inputGear").value = "";
+  document.getElementById("inputFuel").value = "";
+  document.getElementById("inputColor").value = "";
+  document.getElementById("inputMileage").value = "";
+  console.log("Fields cleared!");
+}
